b34b54a3d5690ed35327f14e15951de653c19a4d
insearching <graser1305@gmail.com>
2024-10-22 23:32:34 +0200

    Initial setup

---
+++ app/src/main/java/com/plcoding/cryptotracker/core/domain/CoinDataSource.kt
@@ -0,0 +1,10 @@
+package com.plcoding.cryptotracker.core.domain
+
+import com.plcoding.cryptotracker.core.domain.util.NetworkError
+import com.plcoding.cryptotracker.core.domain.util.Result
+import com.plcoding.cryptotracker.crypto.domain.Coin
+
+
+interface CoinDataSource {
+    suspend fun getCoins(): Result<List<Coin>, NetworkError>
+}
